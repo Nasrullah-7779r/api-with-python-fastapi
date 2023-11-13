868d09fbe8cfbeda38c55755bbf0038181be8ab4
Nasrullah <nasrullahzafar.dae@gmail.com>
2023-11-13 09:41:38 +0500

    login method debugged

--- tests/test_user.py
+++ tests/test_user.py
@@ -42,7 +42,8 @@ def test_login(client, test_user):
     res = client.post("/login", data={"username": test_user["email"], "password": test_user["password"]})
     login_res = schemas.Token(**res.json())
     payload = jwt.decode(login_res.access_token, setting.secret_key, setting.algorithm)
-    id = payload.get("user_id")
-    assert id == test_user["id"]
+    pdb.set_trace()
+    uid = payload.get("id")
+    assert uid == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == status.HTTP_200_OK
